8ea85aa0202b6c65d9296629b31bababf52d79c9
solp721 <easter721@gmail.com>
2026-02-01 01:37:50 +0900

    style : 글로벌 좌우 패딩값 설정

--- way-point/src/globalStyle.ts
+++ way-point/src/globalStyle.ts
@@ -2,6 +2,8 @@ import { StyleSheet } from 'react-native';
 
 export const globalStyle = StyleSheet.create({
 	container: {
+		backgroundColor: 'white',
 		flex: 1,
+		paddingHorizontal: 16,
 	},
 });
